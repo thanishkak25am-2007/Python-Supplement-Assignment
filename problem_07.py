87bae9f7ed5d9988881b16c4cc28803b22b2bc76
thanishkak25am-2007 <thanishka.k_25am@kgkite.ac.in>
2026-01-30 11:56:07 +0530

    promblem_07 solved

--- problem_07.py
+++ problem_07.py
@@ -3,8 +3,8 @@
 
 def factorial(n):
     result = 1
-    for i in range(1, n):
-        result * i
+    for i in range(1, n+1):
+        result *= i
     return result
 
 print(f"Factorial of 5: {factorial(5)}")
